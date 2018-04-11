7b1d02c2e42cd71452455ad6e5a0965462ba7a40
sudhasai13 <saisudha13@gmail.com>
2018-04-11 13:56:22 -0400

    Added Hello.java

---
+++ Hello.java
@@ -0,0 +1,14 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+		System.out.println("Printing Hello 10 times");
+		
+		for(int i=0; i<=10;i++)
+		{
+			System.out.println("Hello");
+		}
+	}
+
+}
